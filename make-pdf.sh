1ed32fef6052ab6ca4637efff8d10ad66ba6d241
Ville Nupponen <ville.nupponen@tuni.fi>
2023-01-20 03:50:09 +0200

    Clean up bash script

--- make-pdf.sh
+++ make-pdf.sh
@@ -5,7 +5,7 @@ cd "$(dirname "$0")/src"
 pdflatex -output-directory=../out main.tex
 
 cd ../out
-makeindex -s ../out/main.ist -t main.glg -o main.gls ../out/main.glo
+makeindex -s main.ist -t main.glg -o main.gls main.glo
 cd ../src
 
 biber --output-directory ../out main
